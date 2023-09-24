90e82e516bbfd6e1d311f8539257cb8bea1e2b3e
Hallie Jenkins <jenkinshj@g.cofc.edu>
2023-09-24 18:23:39 -0400

    Add files via upload

---
+++ assignment1.cjs
@@ -0,0 +1,25 @@
+// Include fs module
+var fileReader = require('fs')
+
+
+// Use fs.readFile() method to read the file
+fileReader.readFile('Hw1_SampleFile.txt', function(err, data){
+    var fileArr = data.toString().split('\n')
+    console.log(`Before Sort: ${fileArr} \n`)
+    var temp;
+    // Bubble sort
+    for (let j = 1; j < fileArr.length; j++) {
+        for (let i = 0; i < fileArr.length; i++) {
+            if(fileArr[i] > fileArr[i+1]){
+                temp = fileArr[i]
+                fileArr[i] = fileArr[i+1]
+                fileArr[i+1] = temp
+            }
+
+        }
+    }
+    console.log(`After Sort: ${fileArr} \n`)
+    
+})
+
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Assignment 1 - JavaScript
+
+The program takes the name of a comma-delimited text file (CSV) as input on the command line, sorts the file by the first field and prints the file.
+
+## Setup
+
+The JavaScript program will be run using `node.js` which is available from https://nodejs.org/en/
+
+## Running
+
+From the command line:
+
+```
+node asignment1
+```
